136908e07cc55c741a9c269ede02c2eb5fb93eff
BIKASH KUMAR YADAV <93582708+bikashkumaryadav@users.noreply.github.com>
2025-07-05 02:55:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int maxSum(vector<int> &arr) {
+        int n=arr.size();
+        int ans=0;
+        int curr=0;
+        for(int i=1;i<n;i++){
+            curr=arr[i]+arr[i-1];
+            ans=max(ans,curr);
+        }
+        return ans;
+    }
+};
